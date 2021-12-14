7d2caaef7cec42177c2a415d51ca68ce461d4f09
elodie-bsst <elodie.bouisset@efrei.net>
2021-12-14 12:49:16 +0100

    alert admin mode

---
+++ static/alert.js
@@ -0,0 +1,4 @@
+const button = document.querySelector("#delbutton");
+button.addEventListener("click", () =>{
+    alert("The implemented login is not completely secure We have nevertheless chosen to be able to redirect the administrator to the admin page via this button. In theory the user should not be able to access this page, only the admin can.");;
+})
